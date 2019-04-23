30eddeb6a3588fd6cd1c0c17de8404b4c415d118
Patrick B <projectpaatt@gmail.com>
2019-04-23 12:00:42 -0700

    cleaner tablename

--- ajax_sql_select_all.php
+++ ajax_sql_select_all.php
@@ -43,8 +43,9 @@ $dbPDO = new PDO(	'pgsql:dbname='.$config['db_data'].
 //table contents
 for ($idx = 0; $idx < count($responsearr['response']['tablelist']);++$idx)
 {
-  $dbstmt2 = $dbPDO->query('SELECT * FROM '.$responsearr['response']['tablelist'][$idx]['table_name'].';');
-  $responsearr['response']['tables'][ $responsearr['response']['tables']['table_name'] ] = $dbstmt2->fetchAll();
+  $tablename = $responsearr['response']['tablelist'][$idx]['table_name'];
+  $dbstmt2 = $dbPDO->query('SELECT * FROM '.$tablename.';');
+  $responsearr['response']['tables'][ $tablename ] = $dbstmt2->fetchAll();
 }
 
 if (isset($_POST['debug']))
