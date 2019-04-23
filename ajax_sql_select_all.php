63acc78abf18cb80097aa3936e6df074572e63f1
Patrick B <projectpaatt@gmail.com>
2019-04-23 11:08:51 -0700

    get actual table contents

--- ajax_sql_select_all.php
+++ ajax_sql_select_all.php
@@ -28,18 +28,25 @@ $dbPDO = new PDO(	'pgsql:dbname='.$config['db_data'].
 								';user='.$config['db_user'].
 								';password='.$config['db_pass']
 								,NULL,NULL,$opt);
-
+//list tables
 	$dbstmt = $dbPDO->query('
                         SELECT *
                       FROM information_schema.tables
                       WHERE table_type = \'BASE TABLE\'
-                      AND table_schema = \'public\' 
+                      AND table_schema = \'public\'
                       ORDER BY table_type, table_name
 	                ');
 	$results = $dbstmt->fetchAll(PDO::FETCH_ASSOC);
 	$responsearr['status'] = $dbstmt->errorInfo();
-	$responsearr['response'] = $results;
-
+	$responsearr['response']['tablelist'] = $results;
+//table contents
+$dbstmt2 = $dbPDO->prepare('  SELECT *
+                              FROM ?;');
+for ($idx = 0; $idx < count($responsearr['response']['tablelist']);++$idx)
+{
+  $dbstmt2->execute(array($responsearr['response']['tablelist']['table_name']));
+  $responsearr['response']['tables'][ $responsearr['response']['tablelist']['table_name'] ] = $dbstmt2->fetchAll();
+}
 
 if (isset($_POST['debug']))
     var_dump($responsearr);
